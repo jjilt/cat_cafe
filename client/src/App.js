68dbce43873bbc5efce63c5f72b00ba62a84a7fd
Joy Nichols <joynichols@joys-air.lan>
2022-09-27 22:23:15 -0400

    cat CRUD done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/shared/home/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Nomatch from './components/shared/Nomatch';
+import MainNavbar from './components/shared/MainNavbar';
+import Footer from './components/shared/Footer';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import Cats from './components/cats/Cats';
+import CatForm from './components/cats/CatForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <MainNavbar />
+    <FetchUser>
+      <>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* protected routes are only for pages accessed when we are logged in */}
+          <Route path='/' element={<ProtectedRoute /> }>
+            <Route path='/cats' element={<Cats />} />
+            <Route path='/:id/updateCat' element={<CatForm />} />
+          </Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/*' element={<Nomatch />} />
+        </Routes>
+      </>
+    </FetchUser>
+    <Footer />
+  </>
+)
 
 export default App;
